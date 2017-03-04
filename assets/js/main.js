a2a4f1d42afbe467ef1f9a6eca311a8ab86e696a
Dana Franco <turquoise.ilg@hotmail.com>
2017-03-04 15:14:40 -0500

    Mi primer commit :)

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function calcularIMC(e){
+
+ var peSo = document.getElementById("peso").value;
+ var esTatura = document.getElementById("estatura").value;
+
+ return alert( parseInt(peSo) + parseInt(esTatura));
+
+
+}
