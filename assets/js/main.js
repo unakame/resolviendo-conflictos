d7b9f5a3bf634edc33e97a45ef8de5c8b89129da
Dana Franco <turquoise.ilg@hotmail.com>
2017-03-05 14:22:15 -0500

    arreglé algunos errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ function calcularIMC(e){
  var peSo = document.getElementById("peso").value;
  var esTatura = document.getElementById("estatura").value;
 
- return alert( parseInt(peSo) + parseInt(esTatura));
-
-
+ var resultado = parseInt(peSo) / Math.pow(esTatura,2);
+ var resultado2 = resultado.toFixed(1);
+   return  alert( "Tu IMC es: " + resultado2);
 }
